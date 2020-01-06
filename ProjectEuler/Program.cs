13e4e943975a8b89e5aaf531f9a1149aa2c6cc17
Simon Toupin <templar_2nd@hotmail.com>
2020-01-05 21:08:33 -0500

    Problem 1

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,9 +4,25 @@ namespace ProjectEuler
 {
     class Program
     {
+        static void ProblemOne()
+        {
+            int sum = 0;
+
+            for (int i = 0; i<1000; i++)
+            {
+                if ( i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Problem 1: {sum}");
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProblemOne();
         }
     }
 }
