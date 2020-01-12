137de40a241aa6da93e1804eb6ff3381c27d4077
Simon Toupin <templar_2nd@hotmail.com>
2020-01-11 20:29:48 -0500

    Problem five

--- ProjectEuler/ProblemFive.cs
+++ ProjectEuler/ProblemFive.cs
@@ -5,9 +5,38 @@ namespace ProjectEuler
 {
     class ProblemFive
     {   
+        static int Max = 20;
         public int Resolve()
         {
+            for(int i=1; i<Int32.MaxValue; i++)
+            {
+                if(entirelyDividable(i))
+                {
+                    //printProof(i);
+                    return i;
+                }
+            }
+
             return 0;
+        }
+
+        void printProof(int num)
+        {
+            for(int i=1; i<=Max; i++)
+            {
+                Console.WriteLine($"{num} / {i} = {num/i}");
+            }
+        }
+
+        bool entirelyDividable(int num)
+        {
+            for(int i=1; i<=Max; i++)
+            {
+                if(num % i != 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
